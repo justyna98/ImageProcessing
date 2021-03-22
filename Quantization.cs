f4d2c4b38797bfd707ec8cda713c1c46fd32ed9e
Justyna <j.pawelec@student.mini.pw.edu.pl>
2021-03-22 18:14:49 +0100

    color

--- Quantization.cs
+++ Quantization.cs
@@ -6,6 +6,29 @@ using System.Threading.Tasks;
 
 namespace task_1
 {
+    class Color
+    {
+        int R;
+        int G;
+        int B;
+        public Color(int r, int g, int b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+      
+        public static int EucliceanDistance(Color c1, Color c2)
+        {
+            int redDifference = c1.R - c2.R;
+            int greenDifference = c1.G - c2.G;
+            int blueDifference = c1.B - c2.B;
+
+            return redDifference * redDifference + greenDifference * greenDifference + blueDifference * blueDifference;
+
+        }
+    }
     class Quantization
     {
 
